2cecd40d2cf97d2848ec9887ef6d6d24e2a63134
kovacik3 <kovalcik3@stud.uniza.sk>
2025-12-03 18:36:22 +0100

    Vytvor zakladny Express server a layout

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.set("views", path.join(__dirname, "..", "views"));
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.urlencoded({ extended: true }));
-
+  
 app.get("/", (req, res) => {
   const nadchadzajuceTreningy = [
     { nazov: "Silový tréning", den: "Pondelok", cas: "18:00" },
